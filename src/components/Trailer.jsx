b932c11d5bcf6ea4be43837466e6c7d2a1abaa83
coderrahul-cyber <rahulsmant299@gmail.com>
2024-10-23 19:12:43 +0530

    dvh

--- src/components/Trailer.jsx
+++ src/components/Trailer.jsx
@@ -15,7 +15,7 @@ function Trailer() {
 
   return ytv != undefined ? (
     <div className='absolute bg-[rgba(0,0,0,0.8)]   w-full h-full '>
-        <div className="w-[100vw] h-[100vh] relative flex items-center justify-center">
+        <div className="w-[100dvw] h-[100dvh] relative flex items-center justify-center">
 
                 <i onClick={() => navigate(-1)} className="ri-close-large-line cursor-pointer absolute sm:top-36 sm:right-[20%] top-[30%]  right-2 text-xl sm:text-3xl text-zinc-300 hover:text-white"></i>
       <ReactPlayer   controls={true}  height={window.innerWidth <= 430 ?300:600} width={1080} url={`https://www.youtube.com/watch?v=${ytv.key}`} />
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,11 +2,11 @@
 function NotFound() {
   return (
     <div className='absolute  w-full h-full bg-[#1F1E24]      text-white '>
-      <div className="flex items-center justify-center w-[100vw] h-[100vh]">
+      <div className="flex items-center justify-center w-[100dvw] h-[100dvh]">
         <h1 className='text-[4vmax] font-fon  '>Not Found</h1>
 
       </div>
-      <div className="flex items-center justify-center bg-[#1F1E24] w-[100vw] h-[100vh]">
+      <div className="flex items-center justify-center bg-[#1F1E24] w-[100dvw] h-[100dvh]">
         <h1 className='text-[4vmax] font-fon   '>Nothing here</h1>
 
       </div>
