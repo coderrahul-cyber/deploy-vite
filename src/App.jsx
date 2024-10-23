afba965f051847b038cf41bd9380f68be526bbaf
coderrahul-cyber <rahulsmant299@gmail.com>
2024-10-23 18:59:02 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 function App() {
 
+  console.log("if The data is not visible be sure you are using the wifi");
   return (
     <div className=''>
       <Outlet/>
